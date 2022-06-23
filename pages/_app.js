8abe55f677f27e1adda68ac747df819cd4eeb7d8
Abd-ul-Ahad <abd-ul-ahad@outlook.com>
2022-06-23 10:24:30 +0500

    top loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../Components/Footer';
+import LoadingBar from 'react-top-loading-bar'
+import { useEffect, useState } from 'react';
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      setProgress(40)
+    })
+    router.events.on('routeChangeComplete', () => {
+      setProgress(100)
+    })
+  }, []);
+
   return (
     <>
+      <LoadingBar
+        color='#E71B1E'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Component {...pageProps} />
       <Footer />
     </>
